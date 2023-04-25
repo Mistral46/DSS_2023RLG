ff6cdf7c535b6a744d5907564eb6267f33fd532a
Mistral46 <reyleal@gmail.com>
2023-04-25 18:24:47 -0400

    Clase4

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Funcionando')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+es6Renderer = require('express-es6-template-engine'),
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
+app.get('/test',(request,response)=>{
+    response.send('testeando')
+})
+
+app.post('/postData',(request,response) =>{
+    console.log(request.body)
+    response.send(`Los datos enviados fueron ${request.body.email} ${request.body.password}`)
+})
+
+app.get('/index',(request,response) =>{
+    response.render('index})
+app.post('/enviarDatos',(request,response) =>{
+    response.render('recibe_datos',{locals: {busqueda : request.body.buscar}})
+})
+
+app.listen(3000,()=>{
+    console.log('Escuchando en el puerto 3000')
+})
